2e48cdc72656f0acc09d997d51d91bb387779aca
elprinced <ccuk@ccs-MacBook-Air.local>
2024-09-16 16:11:31 +0100

    Profile picture update

--- Mock/app/(tabs)/(account)/four.tsx
+++ Mock/app/(tabs)/(account)/four.tsx
@@ -88,39 +88,65 @@ const Profile = () => {
         Alert.alert("Permission to access media library is required!");
         return;
       }
-
+  
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
         aspect: [1, 1],
         quality: 1,
       });
-
+  
       if (!result.canceled) {
         const uri = result.assets[0].uri;
         const formData = new FormData();
-
+  
         const fileName = uri.split("/").pop();
         const fileType = uri.split(".").pop();
-
-        formData.append("profile_picture", {
+  
+        // Create form data for Imgur upload
+        formData.append("image", {
           uri,
           name: fileName,
           type: `image/${fileType}`,
         });
-
-        const config = {
+  
+        const imgurConfig = {
+          headers: {
+            Authorization: "5fa58c7d05ea125",  // Replace with your Imgur client ID
+            "Content-Type": "multipart/form-data",
+          },
+        };
+  
+        // Upload image to Imgur
+        const imgurResponse = await axios.post(
+          "https://api.imgur.com/3/upload",
+          formData,
+          imgurConfig
+        );
+  
+        const imgurLink = imgurResponse.data.data.link;
+  
+        // Update user profile with Imgur image link
+        const userUpdateData = new FormData();
+        userUpdateData.append("profile_picture", imgurLink); // Send URL to the backend
+        
+  
+        const userUpdateConfig = {
           headers: {
             Authorization: `Token ${userToken?.token}`,
             "Content-Type": "multipart/form-data",
           },
         };
 
+        
+  
         const response = await axios.patch(
           `${ApiUrl}/api/update/user/${userInfo?.user.id}/`,
-          formData,
-          config
+          {profile_picture: imgurLink},
+          userUpdateConfig
         );
+  
+        // Update user info in frontend state
         if (userInfo) {
           setUserInfo({
             ...userInfo,
@@ -135,6 +161,8 @@ const Profile = () => {
       console.error("Error updating profile picture:", error);
     }
   };
+  
+  
 
   const styles = StyleSheet.create({
     container: {
